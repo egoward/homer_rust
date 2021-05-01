839a1020ae25e028d7352f8b709935d377c519b1
Ed Goward <ed@goward.com>
2021-05-01 10:52:58 +0100

    Add support for config files

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod ble;
 mod mqtt;
 mod homer_relay;
 
+use structopt::StructOpt;
+use serde::{Serialize, Deserialize};
+
 use homer_relay::core::*;
 use homer_relay::mqtt::*;
-
-use structopt::StructOpt;
-
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Debug,StructOpt)]
@@ -21,6 +21,9 @@ enum Command {
     },
     #[structopt(about = "Run the thing")]
     Run {
+    },
+    #[structopt(about = "Write an example configuration file")]
+    WriteExampleConfig {
     }
 }
 
@@ -37,17 +40,46 @@ struct CommandLine {
 
 
 
+
+#[derive(Deserialize,Serialize,Debug)]
+struct ConfigMain {
+    title: String,
+    mqtt: ConfigMQTT,
+}
+
+#[derive(Deserialize,Serialize,Debug)]
+struct ConfigMQTT {
+    server: String,
+    #[serde(default)]
+    port: u32
+
+}
+
 fn main() {
 
     let args = CommandLine::from_args();
 
     if args.verbose {
-        println!("{:?}",args);
-
+        println!("Verbose mode!")
+        println!("Arguments : {:?}",args);
         println!("Using config from {}", args.config_file);
     }
 
+    let file_result = std::fs::read_to_string(args.config_file);
+    let config_content = match file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
 
+     //let config_content = fileResult.unwrap();
+     let config: ConfigMain = toml::from_str(&config_content).unwrap();
+     if args.verbose {
+        println!("Configuration : {:?}",config);
+     }
 
     match &args.cmd {
         Command::TestMQTT {} => {
@@ -56,6 +88,20 @@ fn main() {
         Command::TestBLE { device: _ } => {
             ble::main_ble();
         }
+        Command::WriteExampleConfig {} => {
+            let example_config = ConfigMain {
+                title : "foo".to_string(),
+                mqtt : ConfigMQTT {
+                    server: "localhost".to_string(),
+                    port: 1234
+                }
+            };
+            let test_output : String = toml::to_string(&example_config).unwrap();
+            let filename = "config.example.toml";
+            println!("Writing example configuration to {}", filename);
+            std::fs::write(filename, test_output).unwrap();
+        }
+
         Command::Run {} => {
 
             let manager = MetricManager {
