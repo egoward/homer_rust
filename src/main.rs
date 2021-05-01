442e4dfb2cd253de36255ee0913f562cc2a42596
Ed Goward <ed@goward.com>
2021-05-01 07:55:38 +0100

    Add structopt command line parsing

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,71 @@ mod homer_relay;
 use homer_relay::core::*;
 use homer_relay::mqtt::*;
 
+use structopt::StructOpt;
+
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Debug,StructOpt)]
+#[structopt(about = "The stupid message relay")]
+enum Command {
+    #[structopt(about = "Test some BLE functionality")]
+    TestBLE {
+        device : String
+    },
+    #[structopt(about = "Test some MQTT functionality")]
+    TestMQTT {
+    },
+    #[structopt(about = "Run the thing")]
+    Run {
+    }
+}
+
+#[derive(Debug,StructOpt)]
+struct CommandLine {
+    #[structopt(name = "config", default_value = "config.toml", long = "config")]
+    config_file: String,
+    //#[structopt(name = "v", long = "verbose")]
+    #[structopt(name = "v", long = "verbose")]
+    verbose: bool,
+    #[structopt(subcommand)]  // Note that we mark a field as a subcommand
+    cmd: Command
+}
+
+
+
 fn main() {
-    println!("This is main");
-    //btle1::test_some_local_function();
 
-    let manager = MetricManager {
-        sources : vec! {
-            Box::new( MetricSourceTest {} )
-        },
-        destinations : vec! {
-            Box::new( MetricDestinationLog {} ),
-            Box::new( MetricDestinationMQTT {} )
+    let args = CommandLine::from_args();
+
+    if args.verbose {
+        println!("{:?}",args);
+
+        println!("Using config from {}", args.config_file);
+    }
+
+
+
+    match &args.cmd {
+        Command::TestMQTT {} => {
+            mqtt::main_mqtt();
         }
-    };
+        Command::TestBLE { device: _ } => {
+            ble::main_ble();
+        }
+        Command::Run {} => {
 
-    manager.run();
+            let manager = MetricManager {
+                sources : vec! {
+                    Box::new( MetricSourceTest {} )
+                },
+                destinations : vec! {
+                    Box::new( MetricDestinationLog {} ),
+                    Box::new( MetricDestinationMQTT {} )
+                }
+            };
+        
+            manager.run();
+        }
 
-    mqtt::main_mqtt();
-
-    //ble::main_ble();
-    
+    }
 }
